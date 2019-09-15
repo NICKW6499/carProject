59b5aa7fe5e1dfe538c5f3bb46884d3751310c1b
NICKW6499 <wrightnick29@gmail.com>
2019-09-15 15:17:42 -0400

    added chassis.java, engine.java

---
+++ src/com/company/Chassis.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public interface Chassis {
+    public final String chassis = "Chassis";
+    public Chassis getChassisType();
+    public void setChassisType(String vehicleChassis);
+
+}
