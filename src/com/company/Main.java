bcad4af870449287bd26b391eff583395e80abf7
NICKW6499 <wrightnick29@gmail.com>
2019-09-22 22:07:03 -0400

    completely stuck right now

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,13 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	int array[][]={{2,2,2},{2,2,0}};
-	for(int i=0;i<2;i++){
-	    for(int j=0;j<3;j++){
-	        if(array[i][j]==0&&j==2)
-	            System.out.print(array[i][j]);
-        }
-    }
-    }
+  public static void main(String[] args) {
+
+	}
 }
--- src/com/company/VehicleChassis.java
+++ src/com/company/VehicleChassis.java
@@ -5,10 +5,12 @@ public class VehicleChassis implements Chassis {
     private String chassisName;
 
     public VehicleChassis() {
+
         this.chassisName = Chassis.chassis;
     }
 
     public VehicleChassis(String chassisName) {
+
         this.chassisName = chassisName;
     }
 
@@ -17,11 +19,13 @@ public class VehicleChassis implements Chassis {
     }
 
     public void setChassisType(String vehicleChassis) {
+
         this.chassisName = vehicleChassis;
     }
 
     public String toString() {
-        return "Chassis Name        : " + this.chassisName;
+
+        return "Chassis Name: " + this.chassisName;
     }
 
 
